efc774e66a52ebf1333e56e8b7db303d56206e83
LeonXuuu <hanliang.xu@vanderbilt.edu>
2023-10-16 12:11:15 -0500

    styled the top bar

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -4,29 +4,71 @@ import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Experience from './pages/Experience';
 import Awards from './pages/Awards';
-import { AppBar, Tabs, Tab } from '@mui/material';
+import { AppBar, Tabs, Tab, ThemeProvider, createTheme } from '@mui/material';
+import './styles.css';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#0D47A1',
+    },
+    secondary: {
+      main: '#FFC107',
+    },
+  },
+  typography: {
+    fontFamily: '"Roboto", sans-serif',
+  },
+  
+});
 
 function App() {
   return (
     <Router>
-      <div>
-        <AppBar position="static">
-          <Tabs>
-            <Tab label="Home" component={Link} to="/" />
-            <Tab label="Projects" component={Link} to="/projects" />
-            <Tab label="Experience" component={Link} to="/experience" />
-            <Tab label="Awards" component={Link} to="/awards" />
-          </Tabs>
-        </AppBar>
+      <ThemeProvider theme={theme}>
+        <div>
+          <AppBar
+            position="static"
+            sx={{
+              backgroundColor: theme.palette.primary.main,
+              height: '4rem'
+            }}
+          >
+            <div style={{ display: 'flex', justifyContent: 'flex-end', width: '100%' }}>
+              <Tabs 
+                sx={{ 
+                  alignItems: 'center',
+                  '& .MuiTab-root': {
+                    fontSize: '1.5 rem',
+                    fontFamily: theme.typography.fontFamily,
+                    minWidth: 'auto',
+                    padding: '0 1rem',
+                    color: theme.palette.secondary.main,
+                    '&:hover': {
+                      color: theme.palette.secondary.dark,
+                    },
+                    '&.Mui-selected': {
+                      color: theme.palette.primary.main,
+                    }
+                  } 
+                }}
+              >
+                <Tab label="Home" component={Link} to="/" />
+                <Tab label="Projects" component={Link} to="/projects" />
+                <Tab label="Experience" component={Link} to="/experience" />
+                <Tab label="Awards" component={Link} to="/awards" />
+              </Tabs>
+            </div>
+          </AppBar>
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/experience" element={<Experience />} />
-          <Route path="/awards" element={<Awards />} />
-        </Routes>
-        
-      </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/experience" element={<Experience />} />
+            <Route path="/awards" element={<Awards />} />
+          </Routes>
+        </div>
+      </ThemeProvider>
     </Router>
   );
 }
--- my-website/src/index.js
+++ my-website/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './styles.css';  // If you've added any styles
+import './styles.css';
 
 ReactDOM.render(
   <React.StrictMode>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
