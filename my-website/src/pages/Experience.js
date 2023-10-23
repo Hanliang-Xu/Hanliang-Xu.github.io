51c6ed5baad3b01b460d8c4bc894c2adf539f926
LeonXuuu <hanliang.xu@vanderbilt.edu>
2023-10-23 11:58:41 -0500

    added some photos and

--- my-website/src/pages/Experience.js
+++ my-website/src/pages/Experience.js
@@ -1,14 +1,35 @@
 import React from 'react';
 
-function Experience() {
+const Experience = () => {
   return (
     <div>
-      <h2>Welcome to My Portfolio</h2>
-      <p>ChangePlusPlus, Software Developer</p>
-      <br />
-      <p>Generation Rise, Web Development Contractor</p>
-      <br />
-      <p>Medical-image Analysis and Statistical Interpretation Lab, Research Engineer </p>
+      <h1>My Experience</h1>
+      
+      <div>
+        <h2>Research Engineer</h2>
+        <p>Medical-image Analysis and Statistical Interpretation Lab</p>
+        <a href="https://www.dropbox.com/scl/fi/r38hoe9597ecu4d5e4nn7/Hanliang_SPIE_8_pages.pdf?rlkey=mpucagha4b4aisz8vat5raju7&dl=0">
+          Link to my paper
+        </a>
+      </div>
+      
+      <div>
+        <h2>Web Developer</h2>
+        <p>Generation Rise</p>
+        <img src="../../public/Generation_Rise.png" alt="Generation Rise" />
+        <p>Description of my role</p>
+        <img src="../../public/Page_Sample.png" alt="Page Sample" />
+        <p>The contact page I coded</p>
+      </div>
+      
+      <div>
+        <h2>Web Developer</h2>
+        <p>Book'em as member in Change++</p>
+        <p>We're still developing our portals!</p>
+        <a href="https://www.changeplusplus.org/">
+          Link to Change++, Vanderbilt's most selective web dev student organization
+        </a>
+      </div>
     </div>
   );
 }
