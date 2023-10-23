4536419e1e911309cb20d4db928dbf9c1e11b2a2
LeonXuuu <hanliang.xu@vanderbilt.edu>
2023-10-23 12:18:39 -0500

    editted the experience page.

--- my-website/src/pages/Experience.js
+++ my-website/src/pages/Experience.js
@@ -1,30 +1,37 @@
+// Experience.js
 import React from 'react';
+import './Experience.css';
 
 const Experience = () => {
   return (
-    <div>
-      <h1>My Experience</h1>
+    <div className="container">
+      <h2>My Experience</h2>
       
-      <div>
-        <h2>Research Engineer</h2>
-        <p>Medical-image Analysis and Statistical Interpretation Lab</p>
+      <div className="experience-item">
+        <p>Research Engineer, Medical-image Analysis and Statistical Interpretation Lab</p>
         <a href="https://www.dropbox.com/scl/fi/r38hoe9597ecu4d5e4nn7/Hanliang_SPIE_8_pages.pdf?rlkey=mpucagha4b4aisz8vat5raju7&dl=0">
           Link to my paper
         </a>
       </div>
-      
-      <div>
-        <h2>Web Developer</h2>
-        <p>Generation Rise</p>
-        <img src="../../public/Generation_Rise.png" alt="Generation Rise" />
-        <p>Description of my role</p>
-        <img src="../../public/Page_Sample.png" alt="Page Sample" />
-        <p>The contact page I coded</p>
+      <br/>
+
+      <div className="experience-item">
+        <p>Web Developer, Generation Rise</p>
+        <div className="img-container">
+          <div>
+            <img src="Generation_Rise.png" alt="Generation Rise" />
+            <p>Description of my role</p>
+          </div>
+          <div>
+            <img src="Page_Sample.png" alt="Page Sample" />
+            <p>The contact page I coded</p>
+          </div>
+        </div>
       </div>
+      <br/>
       
-      <div>
-        <h2>Web Developer</h2>
-        <p>Book'em as member in Change++</p>
+      <div className="experience-item">
+        <p>Web Developer, Book'em as member in Change++</p>
         <p>We're still developing our portals!</p>
         <a href="https://www.changeplusplus.org/">
           Link to Change++, Vanderbilt's most selective web dev student organization
