aed7b51585b55290d97e9113f91db134ff6a79a4
LeonXuuu <hanliang.xu@vanderbilt.edu>
2024-05-29 21:07:59 -0500

    add the backend back in.

--- backend/server.py
+++ backend/server.py
@@ -4,8 +4,8 @@ import os
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 
-from json_validation import JSONValidator
-from validator import *
+from backend.json_validation import JSONValidator
+from backend.validator import *
 
 app = Flask(__name__)
 CORS(app)
