c9c9b9a5cb7aed012a28b93646038699b91e75c6
LeonXuuu <hanliang.xu@vanderbilt.edu>
2024-06-21 01:05:32 -0500

    fix repetitive report.

--- backend/server.py
+++ backend/server.py
@@ -593,10 +593,9 @@ def generate_report(values, combined_major_errors, combined_errors, slice_number
 
   if background_suppression is not None:
     report_lines.append(f"{background_suppression} background suppression")
-    if background_suppression_number_pulses is not None and background_suppression_pulse_time is not None:
+    if (background_suppression_number_pulses is not None and background_suppression_number_pulses
+        != "N/A"):
       report_lines.append(f" with {background_suppression_number_pulses} pulses ")
-      report_lines.append(
-        f"at {format_array(background_suppression_pulse_time)} ms after the start of labeling.")
     elif (background_suppression_number_pulses is not None and background_suppression_number_pulses
           != "N/A"):
       report_lines.append(
